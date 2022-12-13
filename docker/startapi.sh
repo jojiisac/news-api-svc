9749160ca79adb653ac650a9c3fec857b61173e2
Joji Isac <jojiisac@gmail.com>
2022-12-13 14:03:07 +0530

    docker scripts

--- docker/startapi.sh
+++ docker/startapi.sh
@@ -4,10 +4,10 @@ echo "hello web :)"
 if [ -f "/config/configfile" ]; 
 then
     echo "Starting with  supplied  config file  :) "
-    java -jar app.jar --spring.config.location=/config/configfile
+    java -jar /opt/app/app.jar --spring.config.location=/config/configfile
 
 else
 echo "Starting with default config file  :( \n please provide config file"
-     java -jar app.jar  
+     java -jar /opt/app/app.jar
 fi 
 
